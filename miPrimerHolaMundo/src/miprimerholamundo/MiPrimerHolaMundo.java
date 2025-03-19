9f291055d8e700f8619677460d300629075458a2
KevinnC18 <kevincorreal1808@gmail.com>
2025-03-19 11:27:59 -0500

    Add files via upload

---
+++ miPrimerHolaMundo/src/miprimerholamundo/MiPrimerHolaMundo.java
@@ -0,0 +1,28 @@
+
+package miprimerholamundo;
+
+
+public class MiPrimerHolaMundo {
+
+   
+    public static void main(String[] args) {
+        
+        float temperatura = 18.5f;
+        double sueldoBasico = 4000000.56484;
+        Integer numLista = 3;
+        String nombre = "Kevin 1808 /*/* ";
+        char genero = 'H';
+        boolean meCaso = false;
+            
+        System.out.println("El sueldo basico es: "+sueldoBasico);       
+        System.out.println("La temperatura es: "+temperatura);
+        System.out.println("El numero de lista es: "+numLista);
+        System.out.println("El nombre es: "+nombre);
+        System.out.println("El genero es: "+genero);
+        System.out.println("Se casa o no: "+meCaso);
+        
+        System.out.println("Bienvenidos a Java");
+        
+    }
+    
+}
